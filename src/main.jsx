0516ac4d9d49194902fe772172e6ad5dcee64751
Prishi <priyanshimishra670@gmail.com>
2025-06-29 21:08:52 +0530

    Fix GitHub Pages deployment with router basename and actions

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Houseplant-ecommerce">
         <App />
       </BrowserRouter>
     </Provider>
